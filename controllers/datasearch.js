81193dd3869d614ce110d4bac7b9c61f2748676a
anaiscap <anais.capra@3wa.io>
2022-05-10 17:43:53 +0200

    add css

--- controllers/datasearch.js
+++ controllers/datasearch.js
@@ -1,12 +1,6 @@
 import Passenger from '../Models/Passenger';
 
-const findMale = (req, res, next) => {
-    console.log('ca fonctionne');
-    const result  = Passenger.find({ "Sex": male});
-    console.log(result);
-    return res.redirect('/secret');
-   
-} 
+
 
 module.exports = {
     findMale
--- controllers/secret.js
+++ controllers/secret.js
@@ -1,13 +1,13 @@
 import Passenger from '../Models/Passenger';
 
 export default (req, res) => {
-   if (req.params.sex) {
+  if (req.params.sex) {
     Passenger.find({ Sex: { $eq: req.params.sex }}).exec((err ,doc) => {
       res.render('home/secret',{passengers: doc});
     });
 
     return;
-   }
-   res.render('home/secret',{passengers: null}); 
+  }
+  res.render('home/secret',{passengers: null}); 
 
 } 
